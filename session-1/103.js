d6dbcda693da0da428d2fb6f35f914ae035dac6b
utk09-NCL <utkarsh.t0298@gmail.com>
2026-02-15 11:52:12 +0000

    session 2: quick fire

---
+++ session-1/103.js
@@ -0,0 +1,35 @@
+/** Quick-fire 1 */
+// let a = 1;
+// let b = a++;
+// console.log(a, b);
+
+/** --- */
+
+/** Quick-fire 2 */
+// console.log("5" - 3);
+// console.log("5" + 3);
+// console.log("5" - - 3);
+
+/** --- */
+
+/** Quick-fire 3 */
+// console.log(1 < 2 < 3);
+// console.log(3 > 2 > 1);
+
+/** --- */
+
+/** Quick-fire 4 */
+// const arr = [10, 9, 8, 1, 2, 3];
+// console.log(arr.sort());
+
+/** --- */
+
+/** Quick-fire 5 */
+// const a = {};
+// const b = { key: "value" };
+// const c = {};
+//
+// a[b] = "hello";
+// a[c] = "world";
+//
+// console.log(a[b]);
