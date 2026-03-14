65c03c7ee930b541cbc14bea1a34ec3ca973f66d
utk09-NCL <utkarsh.t0298@gmail.com>
2026-03-14 11:40:28 +0000

    feat: session 1 v2

--- session-2/201.js
+++ session-2/201.js
@@ -1,1 +1,53 @@
-// Session 2 - Exercise 1
+// Session 2 (v2) - Exercise 1
+
+/** Quick-fire 1 */
+// var a = 1;
+// function foo() {
+//   console.log("1:", a);
+//   var a = 2;
+//   console.log("2:", a);
+// }
+// foo();
+
+// let a = 1;
+// function foo() {
+//   console.log("1:", a);
+//   let a = 2;
+//   console.log("2:", a);
+// }
+// foo();
+
+/** --- */
+
+/** Quick-fire 2 */
+// const obj = {
+//   name: "outer",
+//   inner: {
+//     name: "inner",
+//     getName() {
+//       return this.name;
+//     }
+//   }
+// };
+
+// console.log("1:", obj.inner.getName());
+// const fn = obj.inner.getName;
+// console.log("2:", fn());
+
+/** --- */
+
+/** Quick-fire 3 */
+// let fns = [];
+// for (let i = 0; i < 3; i++) {
+//   fns.push(() => i * 10);
+//   console.log(`i:${i}`, fns);
+// }
+// fns.forEach(fn => console.log(fn()));
+
+/** --- */
+
+/** Quick-fire 4 */
+// let x = typeof x;
+// console.log(x);
+
+/** --- */
